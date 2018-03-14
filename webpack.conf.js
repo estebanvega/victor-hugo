45e651ba466886f92a39256096e3816293324636
Esteban Vega <esteban.veg4@gmail.com>
2018-03-14 09:13:56 +0100

    Convert webpack config to CommonJS syntax

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,37 +1,40 @@
-import webpack from "webpack";
-import path from "path";
+const webpack = require("webpack");
+const path = require("path");
 
-export default {
-  module: {
-    rules: [
-      {
-        test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader?name=/[hash].[ext]"
-      },
-      {test: /\.json$/, loader: "json-loader"},
-      {
-        loader: "babel-loader",
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        query: {cacheDirectory: true}
-      }
-    ]
-  },
+module.exports = (env, argv) => {
+  return {
+    context: path.resolve(__dirname, "src"),
+    entry: {
+      app: ["./js/app.js"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
+          loader: "file-loader?name=/[hash].[ext]"
+        },
+        {test: /\.json$/, loader: "json-loader"},
+        {
+          loader: "babel-loader",
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          query: {cacheDirectory: true}
+        }
+      ]
+    },
 
-  plugins: [
-    new webpack.ProvidePlugin({
-      "fetch": "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
-    })
-  ],
+    plugins: [
+      new webpack.ProvidePlugin({
+        fetch:
+          "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
+      })
+    ],
 
-  context: path.join(__dirname, "src"),
-  entry: {
-    app: ["./js/app"]
-  },
-  output: {
-    path: path.join(__dirname, "dist"),
-    publicPath: "/",
-    filename: "[name].js"
-  },
-  externals:  [/^vendor\/.+\.js$/]
+    output: {
+      path: path.join(__dirname, "dist"),
+      publicPath: "/",
+      filename: "[name].js"
+    },
+    externals: [/^vendor\/.+\.js$/]
+  };
 };
